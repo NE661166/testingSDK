d1baa38bdf04fa8bf7cda7f9b5ea094525aa73b5
Ng Evon <evon.ng@ifastfinancial.com>
2020-05-15 08:32:23 +0800

    Add login screen

--- src/app/navigator/TestNavigator.js
+++ src/app/navigator/TestNavigator.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ViewA from '../views/ViewA';
 import ViewB from '../views/ViewB';
 import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from 'testapp/src/app/screens/LoginScreen';
+import DashboardScreen from 'testapp/src/app/screens/DashboardScreen';
 // authentication views
 
 const Stack = createStackNavigator();
@@ -9,10 +11,12 @@ const Stack = createStackNavigator();
 export const TestNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="A"
+      initialRouteName="Login"
       screenOptions={{gestureEnabled: false}}>
       <Stack.Screen name="A" component={ViewA} options={{title: 'A'}} />
       <Stack.Screen name="B" component={ViewB} initialParams={{user: 'B'}} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
     </Stack.Navigator>
   );
-}
+};
---
+++ src/app/screens/DashboardScreen.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export default class DashboardScreen extends Component {
+  componentDidMount() {
+    fetch(
+      'https://eibtest.southeastasia.cloudapp.azure.com/clientws/portfolio/overview/details',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.props.route.params.accessToken}`,
+        },
+      },
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Dashboard</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+});
